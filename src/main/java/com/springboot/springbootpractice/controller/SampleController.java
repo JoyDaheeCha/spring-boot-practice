10511b93a658e4baa402b2dd49bf438f147b332d
JoyDaheeCha <daheecha0612@gmail.com>
2021-05-08 12:50:54 +0900

    VO 추가, Lombok 예체 추가(Getter, Setter, ToString)

--- src/main/java/com/springboot/springbootpractice/controller/SampleController.java
+++ src/main/java/com/springboot/springbootpractice/controller/SampleController.java
@@ -1,5 +1,6 @@
 package com.springboot.springbootpractice.controller;
 
+import com.springboot.springbootpractice.domain.SampleVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,16 @@ public class SampleController {
         return "Hello world";
     }
 
-    @GetMapping("/")
-    public String index() {
-        return "index page";
+    @GetMapping("/sample")
+    public SampleVO makeSample() {
+
+        SampleVO vo = new SampleVO();
+        vo.setVal1("v1");
+        vo.setVal2("v2");
+        vo.setVal3("v3");
+
+        System.out.println(vo);
+
+        return vo;
     }
 }
